a15efe97defe993c7b928b4754a2deee0d228e12
lcy0x1 <arthurlcy0x1@gmail.com>
2022-04-01 18:46:39 -0700

    add armor

---
+++ src/main/java/org/xkmc/polaris_rpg/content/armor/package-info.java
@@ -0,0 +1,8 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+
+package org.xkmc.polaris_rpg.content.armor;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
--- src/main/java/org/xkmc/polaris_rpg/init/data/LangData.java
+++ src/main/java/org/xkmc/polaris_rpg/init/data/LangData.java
@@ -5,6 +5,10 @@ import net.minecraft.util.text.TranslationTextComponent;
 import org.xkmc.polaris_rpg.init.PolarisRPG;
 
 public enum LangData {
+	ARMOR_FULL_SUIT("tooltip.armor.full_suit", "When equip full set:"),
+	ARMOR_ELYTRA_FLY("tooltip.armor.elytra_fly", "Enable elytra fly"),
+	ARMOR_CREATIVE_FLY("tooltip.armor.creative_fly", "Enable creative fly"),
+	ARMOR_HOME("tooltip.armor.home", "Return home when dropped in the void")
 	;
 
 	private final String key, def;
